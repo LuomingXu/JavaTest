68b1688936da1b7d5ae9882350d529eca021a44e
LuoMing Xu <24503603+LuomingXu@users.noreply.github.com>
2018-03-29 21:37:15 +0800

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,6 +16,13 @@ public class Main
 {
     public static void main(String[] args)
     {
+        System.err.println("HelloWorld");
+        System.out.println("HelloWorld");
+
+        System.out.println(System.in);
+        System.out.println(System.out);
+        System.out.println(System.err);
+
         String drivename="com.microsoft.sqlserver.jdbc.SQLServerDriver";
         String dburl="jdbc:sqlserver://徐络溟SURFACEPR\\SQLEXPRESS:2537;DatabaseName=JavaTest";
         String SQL="SELECT * FROM JavaTest.dbo.testTable ORDER BY id DESC";
